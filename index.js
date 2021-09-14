aa2f1db3a382dc59cd07a41b30807fe66e02ffe3
jhonvo <jhon26victor@gmail.com>
2021-09-14 02:15:16 -0400

    Adding the math.random option

--- index.js
+++ index.js
@@ -19,3 +19,72 @@ console.log("This is Pizza 3:", pizza3);
 var pizza4 = pizzaOven("thick", "traditional", ["goat"], ["olives", "tomatoes", "onions"])
 console.log("This is Pizza 4:", pizza4);
 
+// Content below copied from the solution of the exercise to practice the random values.
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<=randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(9, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log("This is a random pizza:", randomPizza());  
